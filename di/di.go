5fcf73f573fbcb96feee9bfec781812e96b3fa71
TakuKaneda <kndtaku@gmail.com>
2021-06-19 17:49:37 +0900

    Add mocking

--- di/di.go
+++ di/di.go
@@ -1,14 +1,19 @@
-package di
+package main
 
 import (
-	"fmt"
-	"io"
+    "fmt"
+    "io"
+    "net/http"
 )
 
 func Greet(writer io.Writer, name string) {
-	fmt.Fprintf(writer, "Hello, %s", name)
+    fmt.Fprintf(writer, "Hello, %s", name)
 }
 
-// func main() {
-// 	Greet(os.Stdout, "Elodie")
-// }
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+    Greet(w, "world")
+}
+
+func main() {
+    http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+}
