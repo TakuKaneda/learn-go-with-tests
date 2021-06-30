12f8a5e1c6a9ca195e8d679de5795835788842df
TakuKaneda <kndtaku@gmail.com>
2021-06-30 21:54:39 +0900

    Add filesystem playerstore

--- server/main.go
+++ server/main.go
@@ -7,5 +7,7 @@ import (
 
 func main() {
 	server := NewPlayerServer(NewInMemoryPlayerStore())
-	log.Fatal(http.ListenAndServe(":5000", server))
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
 }
